6de6f8369d8ab36bdb59ee9c306abd16e8d96b97
Macel B <macel.6b@gmail.com>
2025-04-15 10:17:46 -0600

    Se actualiza main.js con boton dropdown que incluye navegación a cada categoría. (Por problemas técnicos estuve hasta el Lunes 14 sin mi cargador de mi laptop, sin la posibilidad de acceder a mis claves y archivos locales)

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const main = document.getElementsByTagName("main").item(0);
 const URLMain = "https://fakestoreapi.com/products/";
+const ulMenu = document.getElementById("ulMenu");
 
-function getData(){
-    fetch(URLMain)
+function getData(cat){
+    const options = {"method": "GET"};
+    fetch(URLMain+ cat, options)
     .then((response) => {
         console.log(response);
         response.json().then((res)=>{
-             // console.log(res.length);
-             // console.log(res[0].rating)
-             createCards(res)
+            main.querySelectorAll(".card").forEach(card => card.remove()); 
+            createCards(res)
         });
     })
     .catch((err)=>{
@@ -19,8 +20,32 @@ function getData(){
     });
 } // getData
 
+function getCategories(){
+    const options = {"method": "GET"};
+    fetch(URLMain+"categories/", options)
+    .then((response) => {
+        response.json().then((res)=>{
+            //  console.log("Categories: ", res);
+            res.forEach((cat)=>{
+                ulMenu.insertAdjacentHTML("afterbegin",
+                `<li><a class="dropdown-item" onclick="getData('category/${(cat.replace("'", "%27"))}');" >${cat}</a></li>`);
+            });
+        });
+    })
+    .catch((err)=>{
+        main.insertAdjacentHTML("beforeend",
+            `<div class="alert alert-danger" role="alert">
+                ${err.message}
+            </div>`);
+    });
+} // getCategories
+
+getCategories();
+getData("");
+
 function createCards(products) {
-    products.forEach(product => {
+    products.forEach((product, index) => {
+        const modalId = `productModal${index}`; 
         main.insertAdjacentHTML("beforeend", `
             <div class="card mb-3" style="max-width: 540px;">
                 <div class="row g-0">
@@ -32,34 +57,36 @@ function createCards(products) {
                             <h5 class="card-title">${product.title}</h5>
                             <p class="card-text">${product.description}</p>
                             <p class="card-text"><strong>$${product.price}</strong></p>
-                        </div>
-                    </div>
-                    <div>
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        ${product.title}
-                        </button>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel"><h1>${product.title}</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${modalId}">
+                                Ver más
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="${modalId}Label" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="${modalId}Label">${product.title}</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            ${product.description}<br>
+                                            <strong>$${product.price}</strong>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="modal-body">
-                                ${product.description}<br>
-                                $${product.price}
-                            </div>
-                        </div>
+
                         </div>
                     </div>
                 </div>
             </div>
         `);
     });
-    }
+}
 // create cards
 // tomar los 20 productos y mandarlos a imprimir en cards dentro del main en html
-getData();
+
+
